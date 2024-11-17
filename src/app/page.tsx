f79f0b5c7a9f7dcd6d8e9e2bb3680b40f0482ade
elephantcrew <154427759+elephantcrew@users.noreply.github.com>
2024-11-17 21:23:27 +0530

    line-svg

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center w-full min-h-screen bg-slate-900">
+      <h1 className="text-8xl text-center text-teal-500 max-w-screen-md text-balance">
+        Reimagined By Sangeethan
+      </h1>
+    </div>
+  );
+}
